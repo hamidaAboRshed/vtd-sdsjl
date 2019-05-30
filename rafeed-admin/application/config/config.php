7c04504dbf593e4d3947ea4e48bdacf03180c88f
Hamida Abo Rshed <hamida.aborshed@gmail.com>
2019-05-30 09:47:04 +0300

    config

--- rafeed-admin/application/config/config.php
+++ rafeed-admin/application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://192.168.1.18/rafeed_system/rafeed-admin/';
+
+$config['base_url']    = "http://rafeed-srv5/rafeed_system/rafeed-admin/";
 //$config['base_url'] = 'http://192.168.1.116:8082/rafeed';
 
 /*
